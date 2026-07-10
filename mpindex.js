14a53d881707bcc2daaf95e31a2d0947ad83fb35
Frelnix <felixdarma1235@gmail.com>
2026-07-10 15:42:38 +0700

    Add files via upload

---
+++ mpindex.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const logoutBtn = document.querySelector(".logout-btn");
+
+    logoutBtn.addEventListener("click", () => {
+        alert("Logout berhasil!");
+    });
+
+});
